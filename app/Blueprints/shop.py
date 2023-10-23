0c057a63fea3e30972cc833770ccc2040e40976e
uTigas <tfgp@ua.pt>
2023-10-23 10:22:34 +0100

    category implementation

--- app/Blueprints/shop.py
+++ app/Blueprints/shop.py
@@ -21,13 +21,15 @@ def productDetails(name):
         if len(query)==0:
             flash(f"Product {name} does not exist.",'danger')
             return products()  
+        category=db.execute("Select CName from Category_Has_Product WHERE PName=?",(query[0][0],)).fetchone()
+
     except db.IntegrityError:
         flash(f"Product {name} does not exist.",'danger')
         return products()  
-    else:
+    if request.method=="POST":
         flash("Item successfully added to the Cart!","info")
         
-    return render_template('product/productDetails.html',size=True,color=True,name=query[0][0],description=query[0][1],price=query[0][3],image="https://img.freepik.com/fotos-premium/o-suricato-suricata-suricatta-ou-suricate-e-um-pequeno-mangusto-encontrado-no-sul-da-africa_208861-941.jpg")
+    return render_template('product/productDetails.html',category=category[0],size=True,color=True,name=query[0][0],description=query[0][1],price=query[0][3],image="https://img.freepik.com/fotos-premium/o-suricato-suricata-suricatta-ou-suricate-e-um-pequeno-mangusto-encontrado-no-sul-da-africa_208861-941.jpg")
 
 
 @bp.route('/products', methods=('GET', 'POST'))
